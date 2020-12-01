4a7dad5898c5397fdfb55a922dbcfb58fdb29467
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-12-01 20:54:05 +0300

    update i18next

--- src/i18n.js
+++ src/i18n.js
@@ -3,21 +3,33 @@ import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+const detectionOptions = {
+    order: ['querystring', 'cookie', 'localStorage', 'sessionStorage', 'navigator', 'path'],
+    lookupFromPathIndex: 0,
+    lookupQuerystring: "lng",
+    lookupCookie: "i18next",
+    lookupLocalStorage: 'i18nextLng',
+    lookupSessionStorage: 'i18nextLng',
+    caches: ['localStorage', 'cookie'],
+    excludeCacheFor: ['cimode'],
+};
+
 export default i18n
     .use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        lng: 'ru',
         backend: {
             /* translation file path */
             loadPath: '/assets/i18n/{{ns}}/{{lng}}.json'
         },
+        detection: detectionOptions,
         fallbackLng: 'en',
         debug: true,
         ns: ['translations'],
         defaultNS: 'translations',
         keySeparator: false,
+        supportedLngs: ['en', 'ru'],
         interpolation: {
             escapeValue: false,
             formatSeparator: ','
